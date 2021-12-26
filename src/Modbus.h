3dba85d37f39a6c2e461b9dd417ec3f5b8a9630e
David M <dmozartandraws@gmail.com>
2021-12-26 02:38:40 +0100

    refactored read function

--- src/Modbus.h
+++ src/Modbus.h
@@ -1,43 +1,7 @@
-#ifndef Modbus_h
-#define Modbus_h
+#ifndef _Modbus_H // Put these two lines at the top of your file.
+#define _Modbus_H // (Use a suitable name, usually based on the file name.)
 
-#include <SoftwareSerial.h>
-
-class Modbus
-{
-private:
-    SoftwareSerial mod;
-    unsigned char RE;
-    unsigned char DE;
-    byte BufferValue[9];
-    byte ModReadBuffer[] = {0x01, 0x04, 0x00, 0x06, 0x00, 0x02, 0x91, 0xCA}; // Current
-
-    int counter = 0;
-
-public:
-    Modbus(unsigned char RE, unsigned char DE, unsigned char RX, unsigned char TX, int baud);
-    unsigned short ModBus_CRC16(unsigned char *buffer, unsigned short len);
-    byte ModbusData();
-    void run();
-    ~Modbus();
-};
-
-Modbus::Modbus(unsigned char RE, unsigned char DE, unsigned char RX, unsigned char TX, int baud)
-{
-    mod(RX, TX);
-    mod.begin(baud);
-    this->RE = RE;
-    this->DE = DE;
-
-    pinMode(RE, OUTPUT);
-    pinMode(DE, OUTPUT);
-}
-
-Modbus::~Modbus()
-{
-}
-
-unsigned short Modbus::ModBus_CRC16(unsigned char *buffer, unsigned short len)
+unsigned short ModBus_CRC16(unsigned char *buffer, unsigned short len)
 {
     unsigned int cur_crc;
     cur_crc = 0xFFFF;
@@ -59,61 +23,6 @@ unsigned short Modbus::ModBus_CRC16(unsigned char *buffer, unsigned short len)
         } while (--i);
     } while (--len);
     return cur_crc;
-}
-
-byte Modbus::ModbusData()
-{
-    byte i;
-
-    // We set driver enable and receiver enable to high in order to send our request
-    digitalWrite(this->DE, HIGH);
-    digitalWrite(this->RE, HIGH);
-    delay(10);
-
-    if (mod.write(ModReadBuffer, sizeof(ModReadBuffer)) == 8)
-    {
-        // We set driver enable and receiver enable to low in order to receive a response from our req
-        digitalWrite(DE, LOW);
-        digitalWrite(RE, LOW);
-
-        for (int k = 0; k < sizeof(BufferValue); k++)
-        {
-            BufferValue[i] = 0;
-        }
-
-        Serial.print("Reading nr: ");
-        Serial.println(++counter);
-
-        for (i = 0; i < sizeof(BufferValue); i++)
-        {
-            // Serial.println(mod.read(), HEX);
-            BufferValue[i] = mod.read();
-        }
-
-        /*
-    for (i = 0; i < sizeof(BufferValue); i++)
-    {
-      Serial.println(BufferValue[i], HEX);
-    }
-*/
-        Serial.println("");
-
-        byte incoming[4] = {BufferValue[6], BufferValue[5], BufferValue[4], BufferValue[3]};
-        // byte incoming[4] = {BufferValue[3], BufferValue[4], BufferValue[5], BufferValue[6]};
-        float volt1 = *((float *)incoming);
-        Serial.print(volt1);
-        Serial.println("V just nu!!!!!!!!!!!!!!!");
-    }
-    return BufferValue[8];
-}
-
-void Modbus::run()
-{
-    while (mod.write(ModReadBuffer, sizeof(ModReadBuffer)) == 8)
-    {
-        ModbusData();
-        delay(1000);
-    }
 }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,8 @@
 #define RE 32 // Connect RE terminal with 32 of ESP
 #define DE 33 // Connect DE terminal with 33 of ESP
 
-// const byte ModReadBuffer[] = {0x01, 0x04, 0x00, 0x00, 0x00, 0x02, 0x71, 0xCB}; // Voltage
-byte ModReadBuffer[] = {0x01, 0x04, 0x00, 0x06, 0x00, 0x02, 0x91, 0xCA}; // Current
+const byte Voltage[] = {0x01, 0x04, 0x00, 0x00, 0x00, 0x02, 0x71, 0xCB}; // Voltage
+const byte Current[] = {0x01, 0x04, 0x00, 0x06, 0x00, 0x02, 0x91, 0xCA}; // Current
 // const byte ModReadBuffer[] = {0x01, 0x04, 0x00, 0x0C, 0x00, 0x02, 0xB1, 0xC8}; // Power
 // const byte ModReadBuffer[] = {0x01, 0x04, 0x00, 0x46, 0x00, 0x02, 0x90, 0x1E}; // Frequency
 // const byte ModReadBuffer[] = {0x01, 0x04, 0x00, 0x48, 0x00, 0x02, 0xF1, 0xDD}; // kwh
@@ -16,25 +16,31 @@ int counter = 0;
 
 SoftwareSerial mod(26, 27); // RX=26 , TX =27
 
-unsigned short ModBus_CRC16(unsigned char *buffer, unsigned short len) {
-    unsigned int cur_crc;
-    cur_crc = 0xFFFF;
-    do {
-        unsigned int i = 8;
-        cur_crc = cur_crc ^ *buffer++;
-        do {
-            if (0x0001 & cur_crc) {
-                cur_crc >>= 1;
-                cur_crc ^= 0xA001;
-            } else {
-                cur_crc >>= 1;
-            }
-        } while (--i);
-    } while (--len);
-    return cur_crc;
+unsigned short ModBus_CRC16(unsigned char *buffer, unsigned short len)
+{
+  unsigned int cur_crc;
+  cur_crc = 0xFFFF;
+  do
+  {
+    unsigned int i = 8;
+    cur_crc = cur_crc ^ *buffer++;
+    do
+    {
+      if (0x0001 & cur_crc)
+      {
+        cur_crc >>= 1;
+        cur_crc ^= 0xA001;
+      }
+      else
+      {
+        cur_crc >>= 1;
+      }
+    } while (--i);
+  } while (--len);
+  return cur_crc;
 }
 
-byte ModbusData()
+float Read(const byte request[], int size)
 {
   byte i;
 
@@ -43,41 +49,20 @@ byte ModbusData()
   digitalWrite(RE, HIGH);
   delay(10);
 
-  if (mod.write(ModReadBuffer, sizeof(ModReadBuffer)) == 8)
+  if (mod.write(request, size) == 8)
   {
     // We set driver enable and receiver enable to low in order to receive a response from our req
     digitalWrite(DE, LOW);
     digitalWrite(RE, LOW);
 
-    for (int k = 0; k < sizeof(BufferValue); k++)
-    {
-      BufferValue[i] = 0;
-    }
-
-    Serial.print("Reading nr: ");
-    Serial.println(++counter);
-
     for (i = 0; i < sizeof(BufferValue); i++)
     {
-      // Serial.println(mod.read(), HEX);
       BufferValue[i] = mod.read();
     }
-
-/*
-    for (i = 0; i < sizeof(BufferValue); i++)
-    {
-      Serial.println(BufferValue[i], HEX);
-    }
-*/
-    Serial.println("");
-
-    byte incoming[4] = {BufferValue[6], BufferValue[5], BufferValue[4], BufferValue[3]};
-    // byte incoming[4] = {BufferValue[3], BufferValue[4], BufferValue[5], BufferValue[6]};
-    float volt1 = *((float *)incoming);
-    Serial.print(volt1);
-    Serial.println("V just nu!!!!!!!!!!!!!!!");
   }
-  return BufferValue[8];
+  byte incoming[4] = {BufferValue[6], BufferValue[5], BufferValue[4], BufferValue[3]};
+  float val = *((float *)incoming);
+  return val;
 }
 
 void setup()
@@ -91,9 +76,13 @@ void setup()
 
 void loop()
 {
-  while (mod.write(ModReadBuffer, sizeof(ModReadBuffer)) == 8)
-  {
-    ModbusData();
-    delay(1000);
-  }
+  Serial.println("");
+  float volt = Read(Voltage, sizeof(Voltage));
+  Serial.print(volt * 0);
+  Serial.println("V");
+  delay(1000);
+  float curr = Read(Current, sizeof(Current));
+  Serial.print(curr);
+  Serial.println("A");
+  delay(3000);
 }
